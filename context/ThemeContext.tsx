daed21bb66a365a9394ba2623018dfd4cb6638f5
Abhinav Raman <abhinavraman99@gmail.com>
2022-07-15 23:53:48 +0530

    Theme persists with localStorage

--- context/ThemeContext.tsx
+++ context/ThemeContext.tsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 
-
 export const THEMES = {
-  LIGHT: "light",
-  DARK: "dark",
-}
+	LIGHT: "light",
+	DARK: "dark",
+};
 
 export const ThemeContext = React.createContext({
 	currentTheme: THEMES.LIGHT,
@@ -14,17 +13,20 @@ export const ThemeContext = React.createContext({
 export const ThemeContextProvider = ({ children }: any) => {
 	const [currentTheme, setCurrentTheme] = useState<string>(THEMES.LIGHT);
 
-	// useEffect(() => {
-	// 	const localUserId = localStorage.getItem("userId") || "";
-	// 	setUserId(localUserId);
-	// 	setIsLoggedIn(localUserId.length > 0 ? true : false);
-	// }, []);
-
-  const setThemeHandler = (theme: string) => {
-    console.log(theme);
+	useEffect(() => {
+		const localUserTheme = localStorage.getItem("user-local-theme") || "";
+    console.log(localUserTheme);
     
-    setCurrentTheme(theme);
-  }
+		setCurrentTheme(localUserTheme.length > 0 ? localUserTheme : THEMES.LIGHT);
+	}, []);
+
+	const setThemeHandler = (theme: string) => {
+		setCurrentTheme((prevTheme) => {
+			const newTheme = prevTheme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT;
+			localStorage.setItem("user-local-theme", newTheme);
+			return newTheme;
+		});
+	};
 
 	return (
 		<ThemeContext.Provider
